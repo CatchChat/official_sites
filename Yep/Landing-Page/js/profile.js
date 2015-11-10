3f5d09fa619b40a47e835c47f6623afd6853271c
Ray <ray@rayps.com>
2015-11-10 15:31:44 +0800

    Data Rendering

--- Yep/Landing-Page/js/profile.js
+++ Yep/Landing-Page/js/profile.js
@@ -1,3 +1,5 @@
+var api, username;
+
 new Zodiac('zodiac', {
   dotColor: '#AADBFA',
   linkColor: '#AADBFA',
@@ -12,4 +14,85 @@ new Zodiac('zodiac', {
   backgroundColor: '#FAFCFD',
   linkDistance: 50,
   linkWidth: 1
+});
+
+username = window.location.pathname.split("/")[1];
+
+api = "https://park.catchchatchina.com/api/v1/users/" + username + "/profile?callback=?";
+
+$.getJSON(api, function(json) {
+  var BDMapKey, BDMapUrl, addSkills, icon, key, ref, value;
+  $('.spiner').remove();
+  $('.avatar').css({
+    'display': 'block',
+    'background-image': "url(" + json.avatar_url + ")"
+  });
+  if (json.badge != null) {
+    $('.badge').css({
+      'display': 'block',
+      'background-image': "url(/img/badge_" + json.badge + ".png)"
+    });
+  }
+  $('.nickname').html(json.nickname);
+  BDMapKey = "P8qeoPmMSc6FKpMvbLKWVrR0";
+  BDMapUrl = "https://api.map.baidu.com/api?v=2.0&ak=" + BDMapKey;
+  $.ajax({
+    url: BDMapUrl,
+    converters: {
+      'text script': function(text) {
+        return text;
+      }
+    },
+    success: function(response) {
+      var regex, result, script;
+      result = response.replace("http://", "https://");
+      regex = /(.*)(javascript" src=")(.*)(\"\>\<\/script\>\'\)\;\}\)\(\)\;)/;
+      script = result.replace(regex, "$3");
+      return $.getScript(script, function() {
+        var geoc, point, time;
+        time = new Date();
+        window.BMap_loadScriptTime = time.getTime();
+        point = new BMap.Point(json.longitude, json.latitude);
+        geoc = new BMap.Geocoder();
+        return geoc.getLocation(point, function(rs) {
+          return $('.location').html(rs.addressComponents.city);
+        });
+      });
+    }
+  });
+  ref = json.providers;
+  for (key in ref) {
+    value = ref[key];
+    if (value != null) {
+      icon = $("." + key);
+      icon.css("display", "inline-block");
+      switch (key) {
+        case "github":
+          icon.attr("href", value.user.html_url);
+          break;
+        case "dribbble":
+          icon.attr("href", value.user.html_url);
+          break;
+        case "instagram":
+          icon.attr("href", "https://instagram.com/" + value.media[0].user.username);
+      }
+    }
+  }
+  $('.intro').html(json.introduction);
+  addSkills = function(className, data) {
+    var index, results, skill;
+    $(className).css('display', 'block');
+    results = [];
+    for (index in data) {
+      skill = data[index];
+      results.push($(className).append($('<div>').addClass('skill').html(skill.name)));
+    }
+    return results;
+  };
+  if (json.master_skills != null) {
+    addSkills(".master", json.master_skills);
+  }
+  if (json.learning_skills != null) {
+    return addSkills(".learning", json.learning_skills);
+  }
 });
--- Yep/Landing-Page/src/coffee/profile.coffee
+++ Yep/Landing-Page/src/coffee/profile.coffee
@@ -13,3 +13,74 @@ new Zodiac 'zodiac',
     backgroundColor: '#FAFCFD'
     linkDistance: 50
     linkWidth: 1
+
+
+# --- USER DATA RENDERING ---
+username = window.location.pathname.split("/")[1]
+api = "https://park.catchchatchina.com/api/v1/users/#{username}/profile?callback=?"
+
+$.getJSON api, (json)->
+
+    # Spiner
+    $('.spiner').remove()
+
+    # Avatar
+    $('.avatar').css
+        'display': 'block'
+        'background-image': "url(#{json.avatar_url})"
+        
+    # Badge
+    if json.badge?
+        $('.badge').css
+            'display': 'block'
+            'background-image': "url(/img/badge_#{json.badge}.png)"
+
+    # Nickname
+    $('.nickname').html json.nickname
+
+    # Location
+    BDMapKey = "P8qeoPmMSc6FKpMvbLKWVrR0"
+    BDMapUrl = "https://api.map.baidu.com/api?v=2.0&ak=" + BDMapKey
+    $.ajax
+        url: BDMapUrl
+        converters: 'text script': (text)-> return text
+        success: (response)->
+            result = response.replace("http://", "https://")
+            regex = /(.*)(javascript" src=")(.*)(\"\>\<\/script\>\'\)\;\}\)\(\)\;)/
+            script = result.replace regex, "$3"
+
+            $.getScript script, ->            
+                time = new Date()
+                window.BMap_loadScriptTime = time.getTime()
+                point = new BMap.Point(json.longitude, json.latitude)
+                geoc = new BMap.Geocoder()
+                geoc.getLocation point, (rs)->
+                    $('.location').html(rs.addressComponents.city)
+
+    # Links
+    for key, value of json.providers
+        if value?
+            icon = $(".#{key}")
+            icon.css "display", "inline-block"
+            switch key
+                when "github"
+                    icon.attr "href", value.user.html_url
+                when "dribbble"
+                    icon.attr "href", value.user.html_url
+                when "instagram"
+                    icon.attr "href", "https://instagram.com/" + value.media[0].user.username
+                # when "behance"
+                    # icon.attr "href", value.
+    
+    # Introduction
+    $('.intro').html json.introduction
+
+    # Skills
+    addSkills = (className, data)->
+        $(className).css('display', 'block')
+        for index, skill of data
+            $(className).append $('<div>').addClass('skill').html(skill.name)
+
+    if json.master_skills? then addSkills(".master", json.master_skills)
+    if json.learning_skills? then addSkills(".learning", json.learning_skills)
+
